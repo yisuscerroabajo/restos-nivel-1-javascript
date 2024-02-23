c99c7047f74b455999c74807c10ddc173d10a098
Jesús Andrés Aguiar Muñoz <156968986+yisuscerroabajo@users.noreply.github.com>
2024-02-23 16:52:16 -0500

    2.0
    Juego del número secreto ultima versión 2.0

---
+++ 2035-logica-programacion-2-Aula2/app.js
@@ -0,0 +1,71 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+  return;
+}
+console.log(intentos);
+
+function verificarIntento() {
+  let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+  console.log(intentos);
+  if (numeroDeUsuario === numeroSecreto) {
+    asignarTextoElemento(
+      `p`,
+      `Acertaste el número en ${intentos} ${intentos === 1 ? `vez` : `veces`}!`
+    );
+    document.getElementById(`reiniciar`).removeAttribute(`disabled`);
+  } else {
+    if (numeroDeUsuario > numeroSecreto) {
+      asignarTextoElemento(`p`, "El número secreto es menor!");
+    } else asignarTextoElemento(`p`, "El número secreto es mayor!");
+  }
+  intentos++;
+  limpiarCaja();
+}
+function limpiarCaja() {
+  let valorCaja = document.querySelector("#valorUsuario");
+  valorCaja.value = "";
+}
+
+function generarNumeroSecreto() {
+  let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+  console.log(numeroGenerado);
+  console.log(listaNumerosSorteados);
+  //Ya sorteamos todos los números?
+  if (listaNumerosSorteados.length == numeroMaximo) {
+    asignarTextoElemento(`p`, `Ya se sortearon todos los números posibles.`);
+  } else {
+    if (listaNumerosSorteados.includes(numeroGenerado)) {
+      return generarNumeroSecreto();
+    } else {
+      listaNumerosSorteados.push(numeroGenerado);
+      return numeroGenerado;
+    }
+  }
+}
+function condicionesIniciales() {
+  asignarTextoElemento("h1", "Juego del número secreto actualizado!");
+  asignarTextoElemento("p", `Indica un número del 1 al ${numeroMaximo}`);
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+}
+
+function reiniciarJuego() {
+  //Restaurar caja
+  limpiarCaja();
+  //Indicar mensaje de inicio
+  //Generar el número aleatorio
+  //Reinicializar el número de intentos.
+  condicionesIniciales();
+  //Deshabilitar el botón de nuevo juego
+  document.querySelector(`#reiniciar`).setAttribute(`disabled`, true);
+}
+//No borrar, literal son las condiciones iniciales.
+condicionesIniciales();
